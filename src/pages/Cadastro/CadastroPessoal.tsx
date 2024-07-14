2ee609b4ee59e73aa946bfe0318fbd55c10de553
athena272 <guilhermera272@gmail.com>
2024-07-14 03:04:02 -0300

    Update CadastroPessoal.tsx

--- src/pages/Cadastro/CadastroPessoal.tsx
+++ src/pages/Cadastro/CadastroPessoal.tsx
@@ -16,10 +16,14 @@ const schemaForm = z.object({
   nome: z.string().min(5, "O nome deve ter ao menos cinco caracteres"),
   email: z.string()
     .min(1, "O campo é obrigatório")
-    .email("O email não é válido"),
+    .email("O email não é válido")
+    .transform((val) => val.toLocaleLowerCase()),
   telefone: z.string(),
   senha: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
   senhaVerificada: z.string().min(1, "Este campo não pode ser vazio"),
+}).refine((dados) => dados.senha === dados.senhaVerificada, {
+  message: "As senhas não coincidem",
+  path: ["senhaVerificada"],
 })
 
 type FormInputTipos = z.infer<typeof schemaForm>
